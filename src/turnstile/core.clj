6ebe85c26039feda398e6ce3e89f014cfa902794
trikitrok <trikitrok@gmail.com>
2017-01-27 14:21:34 +0100

    introducing helper function

--- src/turnstile/core.clj
+++ src/turnstile/core.clj
@@ -2,15 +2,18 @@
   (:require
     [turnstile.control :as control]))
 
+(defn- locked? [turnstile]
+  (= (:state turnstile) :locked))
+
 (defn pass [turnstile]
-  (if (= (:state turnstile) :locked)
+  (if (locked? turnstile)
     (control/sound-alarm! turnstile)
     (control/lock! turnstile))
   (assoc turnstile :state :locked))
 
 (defn insert-coin [turnstile amount]
   (control/store! turnstile amount)
-  (if (= (:state turnstile) :locked)
+  (if (locked? turnstile)
     (control/unlock! turnstile)
     (control/thank-you! turnstile))
   (assoc turnstile :state :unlocked))
